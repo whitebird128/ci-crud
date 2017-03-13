44a5d9c124b0a08b767d02ab280fd15cfc44a77c
whitebird128 <dan_smit@zoho.com>
2017-03-13 11:31:03 +0100

    change sql

--- cicrud.sql
+++ cicrud.sql
@@ -1,21 +1,3 @@
-/*
-SQLyog Enterprise v12.09 (64 bit)
-MySQL - 10.1.13-MariaDB : Database - cicrud
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`cicrud` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `cicrud`;
-
 /*Table structure for table `primo` */
 
 DROP TABLE IF EXISTS `primo`;
